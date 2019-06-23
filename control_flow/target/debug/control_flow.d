76bee624d772e64c99e0769ab71a71fd5f8a9a3e
frogmonkee <51428047+frogmonkee@users.noreply.github.com>
2019-06-22 21:35:56 -0400

    Temperature converter

---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+C:\Users\modyv\Programming\hello_cargo\control_flow\target\debug\control_flow.pdb: C:\Users\modyv\Programming\hello_cargo\control_flow\src\main.rs
---
+++ control_flow/target/debug/deps/rand_os-11b001d72e0779cd.d
@@ -0,0 +1,7 @@
+C:\Users\modyv\Programming\hello_cargo\control_flow\target\debug\deps\librand_os-11b001d72e0779cd.rlib: C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\lib.rs C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\dummy_log.rs C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\windows.rs
+
+C:\Users\modyv\Programming\hello_cargo\control_flow\target\debug\deps\rand_os-11b001d72e0779cd.d: C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\lib.rs C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\dummy_log.rs C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\windows.rs
+
+C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\lib.rs:
+C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\dummy_log.rs:
+C:\Users\modyv\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_os-0.1.3\src\windows.rs:
